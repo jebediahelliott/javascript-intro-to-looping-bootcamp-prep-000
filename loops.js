d0a0f9a471ca1139a8274fb7bd997445d002c72f
Jebediah Elliott <jebediahelliott@gmail.com>
2018-04-29 22:11:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,3 +22,16 @@ function whileLoop(int)
   }
   return "done";
 }
+function doWhileLoop(array)
+{
+  function maybeTrue()
+  {
+    return Math.random() >= 0.5;
+  }
+  do
+  { 
+    array.pop();
+  }
+  while(array.length > 0 && maybeTrue());
+  return array;
+}
